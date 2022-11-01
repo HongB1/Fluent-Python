12755bc684afa757b17b8415c38df341bc877e3c
yesinkim <bailando.ys@gmail.com>
2022-11-02 02:15:37 +0900

    Add Chapter7

---
+++ Yesin/Ch7/registration_param.py
@@ -0,0 +1,23 @@
+registry = set()
+
+def register(active=True):
+    def decorate(func):
+        print(f'running register(active={active}) -> decorate({func})')
+        if active:
+            registry.add(func)
+        else:
+            registry.discard(func)
+
+        return func
+    return decorate
+
+@register(active=False)
+def f1():
+    print('running f1()')
+
+@register()
+def f2():
+    print('running f2()')
+
+def f3():
+    print('running f3()')
