136cdee8f333c7d39d7f9730148315567fb7616a
dongjinlee <94mostp@gmail.com>
2023-01-12 19:08:27 +0900

    Modify Chapter16 and add Chapter17

---
+++ 이동진/Chapter17/flags.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import time
+
+import requests
+
+POP20_CC = ('CN IN US ID BR PK NG BD RU JP '
+            'MX PH VN ET EG DE IR TR CD FR').split()
+BASE_URL = 'http://flupy.org/data/flags'
+DEST_DIR = 'downloads/'
+
+
+def save_flag(img, filename):
+    path = os.path.join(DEST_DIR, filename)
+    with open(path, 'wb') as fp:
+        fp.write(img)
+
+
+def get_flag(cc):
+    url = f"{BASE_URL}/{cc.lower()}/{cc.lower()}.gif"
+    resp = requests.get(url)
+    return resp.content
+
+
+def show(text):
+    print(text, end=' ')
+    sys.stdout.flush()
+
+
+def download_many(cc_list):
+    for cc in sorted(cc_list):
+        image = get_flag(cc)
+        show(cc)
+        save_flag(image, cc.lower() + '.gif')
+
+    return len(cc_list)
+
+
+def main(download_many):
+    t0 = time.time()
+    count = download_many(POP20_CC)
+    elapsed = time.time() - t0
+    msg = f"\n{count} flags downloaded in {elapsed:.2f}s"
+    print(msg)
+
+
+if __name__ == '__main__':
+    main(download_many)
